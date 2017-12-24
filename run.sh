0e81a366b1743e166561cbd078d1749a90237122
MacGyver <macgyver1024@gmail.com>
2017-12-25 00:18:40 +0100

    Sort files by date

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 DOWNLOAD_IMAGES=0
+SORT_IMAGES=1
+CREATE_VIDEO=0
+UPLOAD_VIDEO=0
 
 . config.cfg
-echo Download files from ${ftp_host}...
+
 cd images
 
 if [ ${DOWNLOAD_IMAGES} -eq 1 ]
 then
+echo Download files from ${ftp_host}...
 ftp -inv ${ftp_host} << EOF
     user ${ftp_user} ${ftp_pw}
     cd images
@@ -15,3 +19,13 @@ ftp -inv ${ftp_host} << EOF
 EOF
 fi
 
+cd ../sorted_images
+if [ ${SORT_IMAGES} -eq 1 ]
+then
+echo Sort files into directories
+for file in ../images/*; do
+  DAY="$(echo ${file##*/} | cut -d'_' -f3 | cut -c1-8)"
+  mkdir -p ${DAY}
+  cp ../images/${file##*/} ${DAY}/${file##*/}
+done
+fi
