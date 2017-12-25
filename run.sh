c00a40f50bb7f9b93a3a6791052e05bbd1dac12c
MacGyver <macgyver1024@gmail.com>
2017-12-25 18:16:48 +0100

    Youtube upload

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 DOWNLOAD_IMAGES=0
 SORT_IMAGES=1
 CREATE_VIDEO=1
-UPLOAD_VIDEO=0
+UPLOAD_VIDEO=1
 CLEAN_LOCAL=0
 CLEAN_FTP=0
 
@@ -15,12 +15,6 @@ if [ ${DOWNLOAD_IMAGES} -eq 1 ]
 then
 cd images
 echo Download files from ${ftp_host}...
-#ftp -inv ${ftp_host} << EOF
-#    user ${ftp_user} ${ftp_pw}
-#    cd images
-#    mget *201712*
-#    bye
-#EOF
 wget --user=${ftp_user} --password=${ftp_pw} ftp://${ftp_host}/images/*
 cd ..
 fi
@@ -41,16 +35,28 @@ fi
 if [ ${CREATE_VIDEO} -eq 1 ]
 then
 cd sorted_images
-echo Create videos
+echo Create videos...
 for dir in ./*; do
-  ls ${dir##*/} -1tr > frames.txt
+  ls ${dir##*/} -1 > frames.txt
   cd ${dir##*/}
   mencoder -nosound -ovc lavc -lavcopts \
 vcodec=mpeg4:mbd=2:trell:autoaspect:vqscale=3 \
--vf scale=1920:1080 -mf type=jpeg:fps=20 \
+-vf scale=1920:1080 -mf type=jpeg:fps=10 \
 mf://@../frames.txt -o ../${dir##*/}.avi
   cd ..
   rm frames.txt
+  exit
 done
 cd ..
+fi
+
+if [ ${UPLOAD_VIDEO} -eq 1 ]
+then
+echo Upload videos...
+for dir in sorted_images/*.avi; do
+  DAY="$(echo ${dir##*/} | cut -d'.' -f1)"
+  DAY_DELIMITED="$(echo $DAY | cut -c1-4).$(echo $DAY | cut -c5-6).$(echo $DAY | cut -c7-8)."
+  python upload.py --file="sorted_images/${dir##*/}" \
+  --title="${DAY_DELIMITED}" --description="Időjárás ${DAY_DELIMITED}"
+done
 fi
